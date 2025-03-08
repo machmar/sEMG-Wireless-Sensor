fc07be72465f2dbceb93c39bb103988b59fb39f4
machmar <marekmach24@gmail.com>
2025-03-09 00:03:19 +0100

    initial, off detection works

---
+++ Debug/startup_mspm0g110x_gcc.d
@@ -0,0 +1,4 @@
+# FIXED
+
+startup_mspm0g110x_gcc.o: \
+ C:/ti/mspm0_sdk_2_04_00_06/source/ti/devices/msp/m0p/startup_system_files/gcc/startup_mspm0g110x_gcc.c
---
+++ EMG-BatchelorsThesis.c
@@ -0,0 +1,89 @@
+/*
+ * Copyright (c) 2023, Texas Instruments Incorporated
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * *  Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * *  Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * *  Neither the name of Texas Instruments Incorporated nor the names of
+ *    its contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "ti/driverlib/m0p/dl_core.h"
+#include "ti_msp_dl_config.h"
+#include "ti/devices/msp/m0p/mspm0g110x.h"
+
+uint8_t tmp = 1;
+bool dir = 0;
+
+int main(void)
+{
+    SYSCFG_DL_init();
+
+    // reset the GPIO
+    GPIOA->GPRCM.RSTCTL = (0xB1 << 24) | 0b11;
+    delay_cycles(16);
+    // enable power to the GPIO
+    GPIOA->GPRCM.PWREN = (0x26 << 24) | 0b1;
+    delay_cycles(16);
+    // set the iomux to enable the GPIO
+    IOMUX->SECCFG.PINCM[20] = 0x00000001 | (1 << 7) | (1 << 20); //FUNCTION = 1, PC = 1, DRV = 1
+    IOMUX->SECCFG.PINCM[21] = 0x00000001 | (1 << 7) | (1 << 20); //FUNCTION = 1, PC = 1, DRV = 1
+    IOMUX->SECCFG.PINCM[52] = 0x00000001 | (1 << 7); //FUNCTION = 1 (INAMP ENABLE PIN), PC = 1
+    IOMUX->SECCFG.PINCM[53] = 0x00000001 | (1 << 7) | (1 << 18) | (1 << 19); //FUNCTION = 1 (INAMP ELECTRODE OFF PIN), PC = 1,  INENA = 1, HYSTEN = 1
+    delay_cycles(16);
+    // set gpio as output
+    GPIOA->DOESET31_0 = 0b11 << 10;
+
+    // set inamp enable high
+    GPIOA->DOESET31_0 = 1 << 23;
+    GPIOA->DOUTSET31_0 = 1 << 23;
+    delay_cycles(100); // bit of time to settle
+
+    // set inamp electrode off as input
+    GPIOA->DOECLR31_0 = 1 << 24;
+    delay_cycles(16);
+
+    tmp = 1;
+
+    while (1) {
+        GPIOA->DOUTTGL31_0 = 0b1 << 11;  // blink the yellow
+        if (!dir) {
+            if (tmp < 30) tmp++;
+            else dir = 1;
+        }
+        else {
+            if (tmp > 1) tmp--;
+            else dir = 0;
+        }
+        //delay_cycles(tmp * 100000);
+
+        if (GPIOA->DIN31_0 & 1 << 24) { // lead is off
+            GPIOA->DOUTSET31_0 = 1 << 10;
+        }
+        else { // leads are both on
+            GPIOA->DOUTCLR31_0 = 1 << 10;
+        }
+    }
+}
